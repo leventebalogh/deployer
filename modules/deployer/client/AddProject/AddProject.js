cbf928cf1a8561d0498aacb15fc48d7d5bd46960
Levente Balogh <balogh.levente.hu@gmail.com>
2016-08-29 22:24:54 +0200

    feat(deployer/projects): add page for creating new project

--- modules/deployer/client/AddProject/AddProject.js
+++ modules/deployer/client/AddProject/AddProject.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import request from 'superagent';
 import { connect } from 'react-redux';
 import { Form } from '../../../form-builder';
+import { showSuccess, showError } from '../../../notifications';
+import { addProject } from '../actions';
 
-function AddProject({ config, auth }) {
+function AddProject({ config, auth, onSubmit }) {
     return (
         <div className="add-project">
             <h2 className="add-project-title">Add a new project</h2>
@@ -19,35 +22,38 @@ function getFormConfig() {
                 label: 'Name',
                 type: 'text',
                 required: true,
-                classes: 'add-project-title-input'
+                classes: 'add-project-input'
             },
             {
                 name: 'repo-location',
                 label: 'Repo location',
                 type: 'text',
                 required: true,
-                classes: 'add-project-title-input'
+                classes: 'add-project-input',
+                description: 'A link pointing to a git repository, either on GitHub on your own server.'
             },
             {
                 name: 'server-location',
                 label: 'Server location',
                 type: 'text',
                 required: true,
-                classes: 'add-project-title-input'
+                classes: 'add-project-input'
             },
             {
                 name: 'server-working-directory',
                 label: 'Server working directory',
                 type: 'text',
                 required: true,
-                classes: 'add-project-title-input'
+                classes: 'add-project-input'
             },
             {
                 name: 'commands',
                 label: 'Commands to run on each deploy',
-                type: 'text',
+                type: 'textarea',
                 required: false,
-                classes: 'add-project-title-input'
+                classes: 'add-project-input add-project-commands',
+                placeholder: 'E.g. npm install',
+                description: 'List bash commands you would like to run on each deploy. Separate every command with a new line.'
             },
             {
                 name: 'save',
@@ -60,15 +66,30 @@ function getFormConfig() {
     };
 }
 
-function onSubmit() {
-
-}
-
 const mapStateToProps = (state) => ({
     config: state.config,
     auth: state.auth
 });
 
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (e, formData) => {
+        e.preventDefault();
+        request
+            .post('/rest/projects')
+            .send(formData)
+            .accept('json')
+            .end((err, res) => {
+                if (err && res.body.error) {
+                    dispatch(showError(res.body.error));
+                } else {
+                    dispatch(addProject(res.body));
+                    dispatch(showSuccess('Successfully added project.'));
+                }
+            });
+    }
+});
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(AddProject);
