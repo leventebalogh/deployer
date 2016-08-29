e725387947b31fe641d07634582f66dea8b0092b
Levente Balogh <balogh.levente.hu@gmail.com>
2016-08-29 22:24:37 +0200

    feat(deployer/projects): register projects for the promise-hub

--- modules/deployer/main.js
+++ modules/deployer/main.js
@@ -1,9 +1,25 @@
 const registry = require('core/server/registry');
 const query = require('./server/query.project');
 const promisehub = registry.get('promisehub');
+const Promise = require('bluebird');
 
 // Load APIs
 require('./server/api.project.create');
 require('./server/api.project.get');
 require('./server/api.project.update');
 require('./server/api.project.delete');
+
+// Register promisehub endpoints
+promisehub.register('/rest/projects', options => new Promise((resolve, reject) => {
+    if (options.req.isAuthenticated()) {
+        query.getAll(options.req.user._id).then(projects => {
+            resolve({
+                data: projects
+            });
+        });
+    } else {
+        resolve({
+            data: []
+        });
+    }
+}));
