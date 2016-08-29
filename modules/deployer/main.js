dc8b3f3d8f8f6f785935585f295b276d934bcd52
Levente Balogh <balogh.levente.hu@gmail.com>
2016-08-29 22:24:18 +0200

    feat(deployer/projects): load API endpoints

--- modules/deployer/main.js
+++ modules/deployer/main.js
@@ -1,3 +1,9 @@
 const registry = require('core/server/registry');
 const query = require('./server/query.project');
 const promisehub = registry.get('promisehub');
+
+// Load APIs
+require('./server/api.project.create');
+require('./server/api.project.get');
+require('./server/api.project.update');
+require('./server/api.project.delete');
