1e1b1bc3945a19487b6228b3e9c2efb1abc8908d
Levente Balogh <balogh.levente.hu@gmail.com>
2016-08-29 22:25:03 +0200

    feat(deployer/projects): add page for listing projects

--- modules/deployer/client/Projects/Projects.js
+++ modules/deployer/client/Projects/Projects.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import _ from 'lodash';
+import moment from 'moment';
 
-function Projects({ config, auth }) {
+function Projects({ projects, deleteProject }) {
     return (
-        <div className="deployer-projects">
-            Hello World1
+        <div className="projects">
+            {getEmptyMessage(projects)}
+            {renderProjects(projects, deleteProject)}
         </div>
     );
 }
 
+function renderProjects(projects, deleteProject) {
+    if (_.isArray(projects)) {
+        let i = 0;
+        return projects.map(project => (
+            <div className="project" key={i++}>
+                <div className="project-name">
+                    <span className="project-live ion-record"></span>
+                    {project.name}
+                </div>
+                <div className="project-info">
+                    Last modified: {moment(project.updatedAt).fromNow()}
+                </div>
+                <div className="project-actions">
+                    <Link to={`/projects/${project._id}/edit`} className="circular-button" title="Edit">
+                        <i className="ion-gear-b"></i>
+                    </Link>
+                    <button onClick={deleteProject} className="circular-button" title="Delete">
+                        <i className="ion-trash-a"></i>
+                    </button>
+                    {getLink(project)}
+                </div>
+            </div>
+        ));
+    }
+
+    return '';
+}
+
+function getLink(project) {
+    const link = `/projects/${project._id}`;
+    return (
+        <Link to={link} title="More" className="pure-button">More</Link>
+    );
+}
+
+function getEmptyMessage(projects) {
+    if (!projects.length) {
+        return 'No Projects';
+    }
+
+    return '';
+}
+
 const mapStateToProps = (state) => ({
-    config: state.config,
-    auth: state.auth
+    projects: state.projects.data
 });
 
 export default connect(
