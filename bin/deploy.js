d52700b99642fa3f9bb590ef77f4e51d31949240
Levente Balogh <balogh.levente.hu@gmail.com>
2019-07-06 16:03:05 +0200

    Add main entrypoint

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,23 +1,27 @@
 #! /usr/bin/env node
-require("module-alias/register");
+require('module-alias/register')
 
-const argv = require("yargs").argv;
-const config = require("../config");
-const bash = require("@lib/bash");
-const git = require("@lib/git");
-const deployer = require("@lib/deployer");
+const argv = require('yargs').argv
+const config = require('../config')
+const bash = require('@lib/bash')
+const git = require('@lib/git')
+const deployer = require('@lib/deployer')
 
-const [repoName, branchName] = argv._;
-const { gitBaseUrl, targetFolder } = config;
+const [repoName, branchName] = argv._
+const { gitBaseUrl, checkoutFolderName, checkoutFolder } = config
 
 if (!repoName || !branchName) {
-    bash.logError("Missing parameter.");
-    bash.logError("Correct syntax: $ deploy <repo-name> <branch-name>");
-    bash.exit();
+  bash.logError('Missing parameter.')
+  bash.logError('Correct syntax: $ deploy <repo-name> <branch-name>')
+  bash.exit()
 }
 
-const gitUrl = git.getGitUrl(repoName, gitBaseUrl);
+const gitUrl = git.getGitUrl(repoName, gitBaseUrl)
 
-git.clone(gitUrl, branchName, targetFolder).then(
-    deployer.assertDeployerConfigExists
-);
+git
+  .clone(gitUrl, branchName, checkoutFolder)
+  .then(deployer.assertDeployerConfigExists)
+  .then(deployer.assertDeployerConfigIsValid)
+  .then(() => deployer.deploy({ gitUrl, repoName, branchName, checkoutFolderName }))
+  .then(() => bash.removeFolder(checkoutFolder))
+  .then(() => bash.logSuccess(`✔ "${repoName}" deployed successfully`))
