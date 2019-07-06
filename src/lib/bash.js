526a5dc71480bbbae2cc82ebc30aa7a2655eb9c0
Levente Balogh <balogh.levente.hu@gmail.com>
2019-07-06 16:00:09 +0200

    Lib: add utility functions for Bash

--- src/lib/bash.js
+++ src/lib/bash.js
@@ -1,34 +1,51 @@
-const { exec } = require("child_process");
-const Promise = require("bluebird");
-const chalk = require("chalk");
+const { exec } = require('child_process')
+const Promise = require('bluebird')
+const chalk = require('chalk')
+const fs = require('fs')
+const rimraf = require('rimraf')
 
 module.exports = {
-    command,
-    logInfo,
-    logError,
-    exit
-};
-
-function command(command) {
-    return new Promise((resolve, reject) => {
-        exec(command, (err, stdout) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(stdout);
-            }
-        });
-    });
+  command,
+  logInfo,
+  logError,
+  logSuccess,
+  exit,
+  removeFolder
 }
 
-function exit(code = 0) {
-    process.exit(code);
+function command (command) {
+  return new Promise((resolve, reject) => {
+    const p = exec(command, (err, stdout) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(stdout)
+      }
+    })
+
+    p.stdout.pipe(process.stdout)
+    p.stderr.pipe(process.stderr)
+  })
 }
 
-function logError(msg, ...rest) {
-    console.log(chalk.red(`error: ${msg}`, ...rest));
+function exit (code = 0) {
+  process.exit(code)
 }
 
-function logInfo(msg, ...rest) {
-    console.log(`info: ${msg}`, ...rest);
+function logSuccess (msg, ...rest) {
+  console.log(chalk.green(`success: ${msg}`, ...rest))
+}
+
+function logError (msg, ...rest) {
+  console.log(chalk.red(`error: ${msg}`, ...rest))
+}
+
+function logInfo (msg, ...rest) {
+  console.log(`info: ${msg}`, ...rest)
+}
+
+function removeFolder (folder) {
+  if (fs.existsSync(folder)) {
+    rimraf.sync(folder)
+  }
 }
